0d960a4d5d5f61df764f6d845feeeb8636a5e3fd
Delphine Franquinet <franquinetdelphine@hotmail.com>
2019-06-27 16:59:46 +0200

    création home + route

---
+++ eventCreationAngular/src/events/components/createEvent/createEvent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-createEvent',
+  templateUrl: './createEvent.component.html',
+  styleUrls: ['./createEvent.component.css']
+})
+export class CreateEventComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
