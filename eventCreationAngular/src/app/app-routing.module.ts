d578e6296d43024d05cbdbe6c58177425d9e7912
Zahraa Al-Sudani <zahraobaid1989@gmail.com>
2019-07-01 09:31:22 +0200

    Routing home to createEvent

--- eventCreationAngular/src/app/app-routing.module.ts
+++ eventCreationAngular/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from 'src/events/components/home/home.component';
+import { CreateEventComponent } from '../events/components/createEvent/createEvent.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   {path: 'home' , component: HomeComponent},
+  {path: 'createEvent' , component: CreateEventComponent},
   { path: '**', redirectTo: '/home'}
 ];
 
--- eventCreationAngular/src/app/app.module.ts
+++ eventCreationAngular/src/app/app.module.ts
@@ -4,11 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from 'src/events/components/home/home.component';
+import { CreateEventComponent } from '../events/components/createEvent/createEvent.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    CreateEventComponent
   ],
   imports: [
     BrowserModule,
