9fe12f83216650fb703a0e5421122c07cd6df433
Delphine Franquinet <franquinetdelphine@hotmail.com>
2019-07-01 16:25:05 +0200

    creation class repository

---
+++ eventCreationJava/src/eventCreationJava/Activity.java
@@ -0,0 +1,52 @@
+package eventCreationJava;
+
+import java.sql.Timestamp;
+
+public class Activity {
+	private Integer id;
+	private String name;
+	private String description;
+	private Timestamp startActivity;
+	private Timestamp endActivity;
+	
+	
+	
+	@Override
+	public String toString() {
+		return "Activity [id=" + id + ", name=" + name + ", description=" + description + ", startActivity="
+				+ startActivity + ", endActivity=" + endActivity + "]";
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Timestamp getStartActivity() {
+		return startActivity;
+	}
+	public void setStartActivity(Timestamp startActivity) {
+		this.startActivity = startActivity;
+	}
+	public Timestamp getEndActivity() {
+		return endActivity;
+	}
+	public void setEndActivity(Timestamp endActivity) {
+		this.endActivity = endActivity;
+	}
+	
+	
+
+}
---
+++ eventCreationJava/src/eventCreationJava/Person.java
@@ -0,0 +1,32 @@
+package eventCreationJava;
+
+public class Person {
+	private Integer id;
+	private String name;
+	private String firstname;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + ", firstname=" + firstname + "]";
+	}
+	
+
+}
