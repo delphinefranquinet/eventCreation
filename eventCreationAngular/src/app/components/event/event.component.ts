7111d4c6956bab8d420cdcd69b2cddc96cd4ef73
Romadelf <romadelfatwork@hotmail.com>
2019-08-02 12:20:02 +0200

    Just cleaned the code of "event.component.ts" a bit

--- eventCreationAngular/src/app/components/event/event.component.ts
+++ eventCreationAngular/src/app/components/event/event.component.ts
@@ -3,7 +3,6 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { EventService } from '../../services/event.service';
 import { Activity } from '../../models/activity.modele';
 import { ActivityService } from '../../services/activity.service';
-import { InscriptionActivity } from '../../models/inscriptionActivity.modele';
 import { InscriptionService } from '../../services/inscription.service';
 
 @Component({
@@ -29,75 +28,70 @@ export class EventComponent implements OnInit {
   public activityStart: Date;
   public activityEnd: Date;
 
-  constructor(private router: Router,
-              private route: ActivatedRoute,
-              private inscriptionService: InscriptionService,
-              private activityService: ActivityService,
-              private eventService: EventService) { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private inscriptionService: InscriptionService,
+    private activityService: ActivityService,
+    private eventService: EventService) { }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
       const id: string = params.id;
-
-      this.eventService.getEventByID(id).subscribe(
-        event => {
-          this.eventId = event.id;
-          this.eventName = event.name;
-          this.eventDescreption = event.description;
-          this.eventPlace = event.place;
-          this.eventStart = event.startEvent;
-          this.eventEnd = event.endEvent;
-          this.activities = event.activities;
-
-        }
-      );
+      this.eventService.getEventByID(id).subscribe((event) => {
+        this.eventId = event.id;
+        this.eventName = event.name;
+        this.eventDescreption = event.description;
+        this.eventPlace = event.place;
+        this.eventStart = event.startEvent;
+        this.eventEnd = event.endEvent;
+        this.activities = event.activities;
+      });
     });
-    this.route.params.subscribe(params => {
+    this.route.params.subscribe((params) => {
       const id: string = params.id;
-
-      this.activityService.postActivityByInscriptionID(id).subscribe(
-        activity => {
+      this.activityService.postActivityByInscriptionID(id).subscribe((activity) => {
           // this.activity = activity;
-
           this.activityId = activity.id;
           this.activityName = activity.name;
           this.activityDescreption = activity.description;
           this.activityStart = activity.startActivity;
           this.activityEnd = activity.endActivity;
-
-
-        }
-      );
+        });
     });
-
   }
+
   public inscription(activityId: number) {
-    this.inscriptionService.getInscription(activityId).subscribe(inscription => {
+    this.inscriptionService.getInscription(activityId).subscribe((inscription) => {
       this.connectionError = true;
       this.router.navigate(['/activityInscription']);
     }, () => {
       this.connectionError = false;
     });
-
-
   }
+
+  /****************************************************************************************************
+      Info for Zahraa (by Roman):
+      On this commit, I changed the display of the 2 methods below,but didn't actually changed any content, don't worry ;-)
+  ****************************************************************************************************/
+
+  // public inscription(id: string) {
+  //   if (this.activityError !== null) {
+  //     this.activityService.getInscription(id).subscribe((answer: any) => {
+  //       this.activity = answer;
+  //     });
+  //     this.activityError = true;
+  //   } else {
+  //     this.activityError = false;
+  //   }
+  // }
 
   // public inscriptionActivity(idInscriptionActivity: InscriptionActivity) {
   //   if (idInscriptionActivity === null) {
-
+  //     // Apparently nothing yet...
   //   } else {
   //     this.connectionError = false;
-
   //   }
   // }
-  /*
-      public inscription(id: string) {
-        if ( this.activityError !== null) {
-        this.activityService.getInscription(id).subscribe(answer => this.activity = answer);
 
-          this.activityError = true;
-         } else {
-           this.activityError = false;
-
-         }}*/
 }
