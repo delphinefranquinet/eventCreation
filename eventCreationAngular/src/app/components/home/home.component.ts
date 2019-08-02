9c1883d190392194050a26ed6aa46cf13b42bf06
Zahraa Al-Sudani <zahraobaid1989@gmail.com>
2019-08-02 16:14:28 +0200

    redirecting + back + forward buttons

--- eventCreationAngular/src/app/components/home/home.component.ts
+++ eventCreationAngular/src/app/components/home/home.component.ts
@@ -3,6 +3,9 @@ import { EventService } from '../../../app/services/event.service';
 import { EventManage } from '../../models/eventManage.modele';
 import { ClientSpaceService } from '../../services/clientSpace.service';
 import { Person } from '../../models/person.modele';
+import { EventsAndPersons } from '../../models/EventsAndPersons';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Login } from '../../models/login.modele';
 
 @Component({
   selector: 'app-home',
@@ -10,14 +13,29 @@ import { Person } from '../../models/person.modele';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  public events: EventManage[];
-  public persons: Person[];
 
-  constructor(private eventService: EventService, private clientSpaceService: ClientSpaceService) { }
+  public eventAndPerson: EventsAndPersons = new EventsAndPersons();
+  public connectionError: boolean;
+  public logins: Login;
+
+  constructor(private router: Router,
+    // tslint:disable-next-line: align
+    private eventService: EventService, private clientSpaceService: ClientSpaceService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.eventService.getEvent().subscribe(events => this.events = events);
-    this.clientSpaceService.getPerson().subscribe(persons => this.persons = persons);
+
+    this.eventService.getEventsAndPersons().subscribe((eventAndPerson: any) => {
+      this.eventAndPerson.events = eventAndPerson.events;
+      this.eventAndPerson.persons = eventAndPerson.persons;
+    });
+
+
+    // this.clientSpaceService.getPerson().subscribe(persons => {
+    //   this.eventAndPerson.persons = persons.persons;
+    //   console.log(JSON.stringify(persons.persons));
+    //   console.log(this.eventAndPerson.persons[0].id);
+    // });
+
   }
 
 }
