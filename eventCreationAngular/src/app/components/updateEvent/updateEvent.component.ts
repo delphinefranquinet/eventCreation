b95efd9cae6a87509c5983a3c603a629e2923c0d
Romain Celen <romain.celen3@gmail.com>
2019-08-07 11:09:36 +0200

    update event redirection works

---
+++ eventCreationAngular/src/app/components/updateEvent/updateEvent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-updateEvent',
+  templateUrl: './updateEvent.component.html',
+  styleUrls: ['./updateEvent.component.css']
+})
+export class UpdateEventComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
