3ca668015bf1d2019a8c4efda72517882c4521cb
Romadelf <romadelfatwork@hotmail.com>
2019-08-08 10:59:13 +0200

    Reorganised imports and declarations in app.module.ts && app-routing.module.ts

--- eventCreationAngular/src/app/app-routing.module.ts
+++ eventCreationAngular/src/app/app-routing.module.ts
@@ -1,30 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CreateEventComponent } from './components/createEvent/createEvent.component';
-import { LoginComponent } from '../app/components/login/login.component';
-import { SignInLoginComponent } from './components/signInLogin/signInLogin.component';
-import { ActivityComponent } from './components/activity/activity.component';
-import { RegisterComponent } from './components/register/register.component';
-import { EventComponent } from './components/event/event.component';
-import { ActivityInscriptionComponent } from './components/inscription/activityInscription.component';
+
 import { HomeComponent } from './components/home/home.component';
+import { EventComponent } from './components/event/event.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SignInLoginComponent } from './components/signInLogin/signInLogin.component';
+import { LoginComponent } from './components/login/login.component';
+import { ActivityInscriptionComponent } from './components/inscription/activityInscription.component';
 import { ClientSpaceComponent } from './components/clientSpace/clientSpace.component';
+import { CreateEventComponent } from './components/createEvent/createEvent.component';
 import { UpdateEventComponent } from './components/updateEvent/updateEvent.component';
-
-
+import { ActivityComponent } from './components/activity/activity.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'signInLogin', component: SignInLoginComponent },
-  { path: 'createEvent', component: CreateEventComponent },
-  { path: 'activity/:id', component: ActivityComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'activityInscription/:id', component: ActivityInscriptionComponent },
   { path: 'event/:id', component: EventComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'signInLogin', component: SignInLoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'activityInscription/:id', component: ActivityInscriptionComponent },
   { path: 'clientSpace/:id', component: ClientSpaceComponent },
+  { path: 'createEvent', component: CreateEventComponent },
   { path: 'updateEvent/:id', component: UpdateEventComponent },
+  { path: 'activity/:id', component: ActivityComponent },
   { path: '**', redirectTo: '/home' }
 ];
 
--- eventCreationAngular/src/app/app.module.ts
+++ eventCreationAngular/src/app/app.module.ts
@@ -1,54 +1,58 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CreateEventComponent } from './components/createEvent/createEvent.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { LoginComponent } from '../app/components/login/login.component';
-import { SignInLoginComponent } from './components/signInLogin/signInLogin.component';
+
+import { RegisterService } from './services/register.service';
 import { LoginService } from './services/login.service';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { PersonService } from './services/person.service';
 import { EventService } from './services/event.service';
-import { ActivityComponent } from './components/activity/activity.component';
-import { RegisterComponent } from './components/register/register.component';
 import { ActivityService } from './services/activity.service';
-import { RegisterService } from './services/register.service';
-import { EventComponent } from './components/event/event.component';
-import { ClientSpaceComponent } from './components/clientSpace/clientSpace.component';
-import { ActivityInscriptionComponent } from './components/inscription/activityInscription.component';
-import { HomeComponent } from './components/home/home.component';
-import { UpdateEventComponent } from './components/updateEvent/updateEvent.component';
 
+import { HomeComponent } from './components/home/home.component';
+import { EventComponent } from './components/event/event.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SignInLoginComponent } from './components/signInLogin/signInLogin.component';
+import { LoginComponent } from './components/login/login.component';
+import { ActivityInscriptionComponent } from './components/inscription/activityInscription.component';
+import { ClientSpaceComponent } from './components/clientSpace/clientSpace.component';
+import { CreateEventComponent } from './components/createEvent/createEvent.component';
+import { UpdateEventComponent } from './components/updateEvent/updateEvent.component';
+import { ActivityFormComponent } from './components/forms/activity-form/activity-form.component';
+import { ActivityComponent } from './components/activity/activity.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    CreateEventComponent,
-    LoginComponent,
-    SignInLoginComponent,
-    ActivityComponent,
-    RegisterComponent,
     EventComponent,
-    ClientSpaceComponent,
+    RegisterComponent,
+    SignInLoginComponent,
+    LoginComponent,
     ActivityInscriptionComponent,
-    UpdateEventComponent
+    ClientSpaceComponent,
+    CreateEventComponent,
+    UpdateEventComponent,
+    ActivityFormComponent,
+    ActivityComponent
    ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule,
     FormsModule,
     ReactiveFormsModule
   ],
   providers: [
     HttpClient,
+    RegisterService,
     LoginService,
     PersonService,
     EventService,
-    ActivityService,
-    RegisterService
+    ActivityService
   ],
   bootstrap: [
     AppComponent
