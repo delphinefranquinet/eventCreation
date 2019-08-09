fac4d8a7d08d0115ae87c27845cf48369c11c08b
Romadelf <romadelfatwork@hotmail.com>
2019-08-09 14:17:10 +0200

    update-activity component files autogeneration

--- eventCreationAngular/src/app/components/activity/activity.component.ts
+++ eventCreationAngular/src/app/components/activity/activity.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Output } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 import { Activity } from '../../models/activity.model';
 import { ActivityService } from '../../services/activity.service';
@@ -21,7 +21,6 @@ export class ActivityComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private router: Router,
     private activityService: ActivityService
   ) { }
 
